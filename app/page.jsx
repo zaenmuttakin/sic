dc9b0b183af42f8ea76cf685881ea495eb747cf3
Zaen Muttakin <53005403+zaenmuttakin@users.noreply.github.com>
2025-08-01 17:18:29 +0000

    back to manual theme

--- app/page.jsx
+++ app/page.jsx
@@ -1,31 +1,23 @@
 "use client";
-import { Button, Label, TextInput } from "flowbite-react";
 import { useRouter } from "next/navigation";
+import Inputz from "../components/input/input";
+import PrimaryBtn from "../components/button/primary-btn";
 
 export default function Home() {
   const router = useRouter();
   return (
     <div className="flex h-screen items-center justify-center px-12">
       <form className="flex flex-col gap-4 max-w-xs w-full">
-        <div>
-          <div className="mb-1 block">
-            <Label htmlFor="email1">NIK</Label>
-          </div>
-          <TextInput id="email1" type="number" required />
-        </div>
-        <div>
-          <div className="mb-1 block">
-            <Label htmlFor="password1">Password</Label>
-          </div>
-          <TextInput id="password1" type="password" required />
-        </div>
-        <Button
+        <Inputz type="number" placeholder="Username" />
+        <Inputz type="password" placeholder="Password" />
+        <PrimaryBtn
           type="submit"
-          className="mt-6"
-          onClick={() => router.push("/private")}
-        >
-          Login
-        </Button>
+          label="Login"
+          onClick={(e) => {
+            e.preventDefault();
+            router.push("/private");
+          }}
+        />
       </form>
     </div>
   );
--- app/private/page.jsx
+++ app/private/page.jsx
@@ -1,40 +1,25 @@
 "use client";
 import React from "react";
-import { Avatar } from "flowbite-react";
-import { Card } from "flowbite-react";
+
 import { useRouter } from "next/navigation";
+import ProfileImg from "../../components/image/profile";
 
 export default function Page() {
   const router = useRouter();
   return (
     <div className="flex h-screen items-start justify-center w-full">
-      <div className="flex flex-col gap-4 w-full max-w-2xl px-6">
-        {/* topbar */}
-        <div
-          id="topbar"
-          className=" flex justify-between items-center w-full py-4"
-        >
-          <div className="">
-            <p>Hi zaen</p>
-          </div>
-          <Avatar
-            img="https://i.pinimg.com/1200x/4d/52/d1/4d52d11cdbbd556de5f9940bdc76004c.jpg"
-            rounded
-          />
+      {/* Topbar */}
+      <div className="flex gap-2 justify-between items-center w-full max-w-4xl p-6 ">
+        <div className="">
+          <p className=" font-bold">Hi, Difa Ikhsan Egistiyo</p>
+          <p className="text-xs text-gray-500 mt-1">Welcome back 🔥</p>
         </div>
-        {/* <div className="h-full border py-4">Banner</div> */}
-        <div className="h-full py-4">
-          <div className="grid grid-cols-2 gap-4">
-            <Card
-              className="max-w-sm rounded-xl"
-              onClick={() => router.push("/private/so")}
-            >
-              <p className="text-md font-bold tracking-tight text-gray-900 dark:text-white">
-                Stock Opname
-              </p>
-            </Card>
-          </div>
-        </div>
+        <ProfileImg
+          src="https://i.pinimg.com/736x/99/73/63/997363c033991ca3fe13c8f554e88289.jpg"
+          alt="Profile Image"
+          width={50}
+          height={50}
+        />
       </div>
     </div>
   );
