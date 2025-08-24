65a94bc74f1885784d08c923aba224c0b630af73
Zaen Muttakin <53005403+zaenmuttakin@users.noreply.github.com>
2025-08-24 03:30:20 +0000

    manual push

--- app/private/page.jsx
+++ app/private/page.jsx
@@ -7,19 +7,33 @@ import ProfileImg from "../../components/image/profile";
 export default function Page() {
   const router = useRouter();
   return (
-    <div className="flex h-screen items-start justify-center w-full">
+    <div className="flex h-screen items-start justify-center w-full p-6">
       {/* Topbar */}
-      <div className="flex gap-2 justify-between items-center w-full max-w-4xl p-6 ">
+
+      <div className="flex gap-2 justify-between p-4 px-10 items-center w-full bg-white rounded-3xl">
         <div className="">
-          <p className=" font-bold">Hi, Difa Ikhsan Egistiyo</p>
-          <p className="text-xs text-gray-500 mt-1">Welcome back 🔥</p>
+          <p>SIC-Central</p>
         </div>
-        <ProfileImg
-          src="https://i.pinimg.com/736x/99/73/63/997363c033991ca3fe13c8f554e88289.jpg"
-          alt="Profile Image"
-          width={50}
-          height={50}
-        />
+        <div className="flex gap-2 rounded-full bg-[#eeeff4] border px-4 py-2">
+          <p className="">Dashboard</p>
+          <p>Menu</p>
+          <p>Sheets</p>
+          <p>Profile</p>
+        </div>
+        <div className="flex items-center gap-3">
+          <ProfileImg
+            src="https://i.pinimg.com/736x/99/73/63/997363c033991ca3fe13c8f554e88289.jpg"
+            alt="Profile Image"
+            width={50}
+            height={50}
+          />
+          <div className="hidden lg:flex flex-col gap-0 pl-2">
+            <p className="m-0 text-sm">Difa Egi Listianto</p>
+            <span className="m-0 text-xs text-gray-500 mt-1">
+              Technical Store Central
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
