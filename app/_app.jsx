e4daedc0a1a978475729f012e8b6bb13fb52a590
zaenmuttakin <zaenmuttakin@gmail.com>
2025-08-28 21:28:32 +0700

    feat: Export MyApp component as default for improved module usage

--- app/_app.jsx
+++ app/_app.jsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   const headColor = "#faf5ff";
   return (
     <>
