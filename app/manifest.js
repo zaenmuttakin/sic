1b1fadaa9fe6428fed1fe9fa1b879e437bb252b5
zaenmuttakin <zaenmuttakin@gmail.com>
2026-04-03 13:23:11 +0700

    init nextjs

---
+++ app/manifest.js
@@ -0,0 +1,23 @@
+export default function manifest() {
+  return {
+    name: "SIC S4",
+    short_name: "SIC",
+    description: "Sparepart Inventory Control System",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#000000",
+    icons: [
+      {
+        src: "/web-app-manifest-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/web-app-manifest-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
